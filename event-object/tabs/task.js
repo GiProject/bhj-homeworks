6bb9a95d5c00bda34d394fbde6671c0e1b6b6040
Evgeniy Kulinichev <gekin010@ya.ru>
2023-01-19 19:37:19 +0300

    tabs bugfix

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -2,7 +2,6 @@ let tab_links = document.querySelectorAll('.tab__navigation .tab');
 let tab_contents = document.querySelectorAll('.tab__contents  .tab__content');
 tab_links.forEach((tab) => {
    tab.addEventListener('click', (e) => {
-      e.currentTarget.classList.add('tab_active');
       nextTab(Array.from(tab_links).indexOf(e.currentTarget));
    });
 });
@@ -11,5 +10,7 @@ function nextTab(index)
 {
    document.querySelector('.tab_active').classList.remove('tab_active');
    document.querySelector('.tab__content_active').classList.remove('tab__content_active');
+   
    Array.from(tab_contents)[index].classList.add('tab__content_active');
+   Array.from(tab_links)[index].classList.add('tab_active');
 }
